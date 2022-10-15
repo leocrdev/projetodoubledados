227c9250e086bf0e58159d1ab9e897268e60f057
LEOCRDEV leocrdev@hotmail.com <LEOCRDEV leocrdev@hotmail.com>
2022-10-15 19:47:58 -0300

    adicionando projeto de dados com double

---
+++ doubledados/src/doubledados/auladoubledados.java
@@ -0,0 +1,18 @@
+package doubledados;
+
+public class auladoubledados {
+
+	
+	/*Main é um metodo auto executável em Java*/
+	public static void main(String[] args) {
+		double nota1 = 90;
+		double nota2 = 70;
+		double nota3 = 50;
+		double nota4 = 88;
+		
+		double mediaFinal = (nota1 + nota2 + nota3+ nota4) /4;
+		
+	System.out.println("Média de notas = " + mediaFinal);
+		
+	}
+}
